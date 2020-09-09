2aa1cfa83394d6b1511b77ac318160a8f6398e2d
Ethan Gallant <ethan@exclnetworks.com>
2020-09-09 00:00:48 -0400

    Setup socket for cosmosdb

--- socket.js
+++ socket.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 const schedule = require("node-schedule");
-
-let timers = new Map();
+const TimerHelper = require("./database/timer");
 let notifications = new Map();
 
+let timerHelper = new TimerHelper();
 
 exports.init = (io) => {
     io.origins((origin, callback) => {
@@ -11,62 +11,76 @@ exports.init = (io) => {
     });
 
     io.on('connection', socket => {
-        socket.emit("timers", Array.from(timers))
-        console.log("Client " + socket.id + " connected!")
+        socket.emit("timers", Array.from(timers));
+        socket.emit('time-sync', new Date().getTime());
 
-        socket.on('reset-timer', timerName => {
-            //let fifteen = new Date().getTime() + (10 * 1000);
-            let fifteen = new Date().getTime() + (15 * 60 * 1000);
-            timers.set(timerName, fifteen)
-            scheduleNotification(timerName, fifteen);
-            io.emit("timers", Array.from(timers))
-        })
+        socket.on('reset-timer', async id => {
+            const timer = await timerHelper.getByID(id);
+            if(!timer)
+                return;
+            timer.expires_at = await new Date().getTime() + ( timer.length * 1000);
+            timerHelper.update(timer);
+            scheduleNotification(timer.id, timer.expires_at);
 
-        socket.on('zero-timer', timerName => {
-            timers.set(timerName, new Date().getTime())
-            io.emit("timers", Array.from(timers))
+            io.emit("timer-update", timer)
+        });
+
+        socket.on('zero-timer', async id => {
+            const timer = await timerHelper.getByID(id);
+            if(!timer)
+                return;
+
+            timer.expires_at = null;
+            timerHelper.update(timer);
+            cancelNotification(timer.name);
+            io.emit("timer-update", timer);
+        });
+
+        socket.on('delete-timer', id => {
+            timerHelper.delete(id);
+            io.emit("delete-timer", id);
             cancelNotification(timerName);
-        })
+        });
 
-        socket.on('delete-timer', timerName => {
-            timers.delete(timerName);
-            io.emit("timers", Array.from(timers))
-            cancelNotification(timerName);
+        socket.on('get-time', () => {
+            socket.emit('time-sync', new Date().getTime());
         })
-
     })
 
-}
+};
 
 function scheduleNotification(name, time) {
     cancelNotification(name);
     let job = setTimeout(() => {
         sendNotification(name);
-    }, time - new Date().getTime())
-    notifications.set(name,job)
+    }, time - new Date().getTime());
+    notifications.set(name, job)
 }
 
 function cancelNotification(name) {
     let notification = notifications.get(name);
-    if (notification && notification != null && notification !== undefined) {
+    if (notification) {
         clearTimeout(notification)
     }
 }
 
-function sendNotification(name) {
+async function sendNotification(id) {
+    const timer = await timerHelper.getByID(id);
+    if(!timer)
+        return;
     axios({
         method: 'post',
         url: process.env.PUSHER_URL,
         headers: {
             "Authorization": process.env.PUSHER_KEY,
-            "Content-Type" : "application/json"
+            "Content-Type": "application/json"
         },
         data: {
             "interests": ["timers"],
-            "web":{
-                "notification":{
-                    "title": "Timer " + name + " Expired.",
-                    "body": "Timer " + name + " has completed. Click here to view timer status.",
+            "web": {
+                "notification": {
+                    "title": "Timer " + timer.name + " Expired.",
+                    "body": "Timer " + timer.name + " has completed. Click here to view timer status.",
                     "deep_link": "https://edctimer.azurewebsites.net/"
                 }
             }
