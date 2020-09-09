901aae8bbee72ef7d3a34e7b3da0eac68564d415
Ethan Gallant <ethan@exclnetworks.com>
2020-09-09 17:08:25 -0400

    Add offset functionality and clean up store methods

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,27 +1,62 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    websocket: null,
-    edit: true,
-    timers: [],
-    
+    state: {
+        websocket: null,
+        edit: true,
+        timers: [],
+        offset: 0,
+    },
+    mutations: {
 
-  },
-  mutations: {
+        setWebsocket(state, val) {
+            state.websocket = val
+        },
 
-    setWebsocket(state, val) {state.websocket = val},
-    setTimers(state, val) {state.timers = val},
-    setEdit(state, val) {state.edit = val},
+        deleteTimer(state, val) {
+            state.timers = state.timers.filter((obj) => {
+                return obj.id !== val;
+            })
+        },
+
+        async updateTimer(state, val) {
+            let timerIndex = await state.timers.findIndex((obj => obj.id === val.id));
+            if (timerIndex < 0) {
+                state.timers.push(val);
+            } else {
+                state.timers[timerIndex] = val;
+            }
+        },
+
+        setTimers(state, val) {
+            state.timers = val
+        },
+
+        setOffset(state, val) {
+            state.timers = val
+        },
+
+        setEdit(state, val) {
+            state.edit = val
+        },
 
 
-  },
-  getters: {
-    getWebsocket: state => {return state.websocket},
-    getTimers: state => {return state.timers},
-    getEdit: state => {return state.edit},
-  },
+    },
+    getters: {
+        getWebsocket: state => {
+            return state.websocket
+        },
+        getTimers: state => {
+            return state.timers
+        },
+        getEdit: state => {
+            return state.edit
+        },
+        getOffset: state => {
+            return state.offset
+        },
+    },
 })
